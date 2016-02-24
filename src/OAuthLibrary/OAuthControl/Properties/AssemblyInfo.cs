418d7721a10b6e80d50785615a89770fe7977d72
unknown <Tim.Smith@notamac.i3domain.inin.com>
2016-02-24 10:17:46 -0700

    updating assembly info

--- src/OAuthLibrary/OAuthControl/Properties/AssemblyInfo.cs
+++ src/OAuthLibrary/OAuthControl/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OAuthControl")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PureCloudOAuthControl")]
+[assembly: AssemblyDescription("PureCloud OAuth Control")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OAuthControl")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Interactive Intelligence, Inc.")]
+[assembly: AssemblyProduct("PureCloud OAuth Control")]
+[assembly: AssemblyCopyright("Copyright © Interactive Intelligence, Inc. 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
