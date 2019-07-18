e978e55024e6ac9a0d341cc12ce381e0edc0af58
Tim Smith <tim.j.smith@genesys.com>
2019-07-18 14:38:05 -0600

    bump version

--- src/OAuthLibrary/OAuthControl/Properties/AssemblyInfo.cs
+++ src/OAuthLibrary/OAuthControl/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PureCloudOAuthControl")]
 [assembly: AssemblyDescription("PureCloud OAuth Control")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Interactive Intelligence, Inc.")]
+[assembly: AssemblyCompany("Genesys")]
 [assembly: AssemblyProduct("PureCloud OAuth Control")]
-[assembly: AssemblyCopyright("Copyright © Interactive Intelligence, Inc. 2016")]
+[assembly: AssemblyCopyright("Copyright © Genesys 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
